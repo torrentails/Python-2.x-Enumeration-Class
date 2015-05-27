e9c8dfaebf2505ed8fa330b857e8b296673a47f1
Nathan Sullivan <torrentails@gmail.com>
2015-05-27 09:47:28 +0930

    Innitial commit.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Python-2.x-Enumeration-Class
-Provides an effective enumeration class with versitle features
+# Python 2.x Enumeration Class
+Provides an effective enumeration class with versatile features
+
+## Licence
+The Enum class is Copyright (c) 2015 Nathan Sullivan of torrentails.com and is licensed under the GNU Lesser General Public License version 3
+
+## Usage
+
+    Enum        ::= "Enum" "(" name "," items ")"
+    name        ::= string
+    items       ::= item | item_list
+    item_list   ::= "[" item "]"
+    item        ::= string [("," item)]
+
+An Enum is an object that takes a string name as it's first parameter and either a list of strings or all remaining arguments are strings, and returns itself in order to be assigned to a variable.
+
+    >>> enum_object = Enum ("enum name", ["item1", "item2", ...])
+    >>> enum_object = Enum ("enum name", "item1", "item2", ...)
+
+the list/args passed become the enumeration's items and can be referenced by parameters or dictionary item reference, and are case insensitive.
+
+    >>> enum_object.item1
+    <Enum_Item object at 0x028E3630>
+    >>> enum_object["iTeM2"]
+    <Enum_Item object at 0x028E3730>
+
+Each Enum item is guaranteed to be unique as each item is an object in memory and will not compare equal with anything other than itself.
+
+    >>> enum_object.item1 == "item1"
+    False
+    >>> enum_object.item1 == enum_object.item2
+    False
+    >>> enum_object.item1 == enum_object["item1"]
+    True
+
+Attempting to set values in an enumeration after its creation will throw an error.
+
+    >>> enum_object.item1 = "Foo"
+    NotImplementedError: 'Enum' object does not support attribute assignment
+    >>> enum_object["item1"] = "Foo"
+    TypeError: 'Enum' object does not support item assignment
+
+Each item in the enumeration can return its name with the `name` attribute.
+
+    >>> enum_object.item1.name
+    'item1'
+
+The name of the enumeration object can be recalled through the use of the `__name__` method. The enumeration object's name can also be referenced via an item's `type` attribute.
+
+    >>> enum_object.__name__()
+    'enum name'
+    >>> enum_object.item1.type
+    'enum name'
---
+++ enum_class.py
@@ -0,0 +1,126 @@
+#-----------------------------------------------------------------------
+#   Python 2.x Enumeration class
+#   Copyright (c) 2015 Nathan Sullivan (email: contact@torrentails.com)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this program.
+#   If not, see http://www.gnu.org/licenses/lgpl
+#-----------------------------------------------------------------------
+
+##----------------------------------------------------------------------
+##  
+##                              Usage
+##  
+##  Enum    ::= Enum "(" name "," items ")"
+##  name    ::= string
+##  items   ::= item | item_list
+##  item_list   ::= "[" item "]"
+##  item    ::= string [("," item)]
+##  
+##  An Enum is an object that takes a sting name as it's first parameter
+##  and either a list of strings or all remaining arguments are strings,
+##  and returns itself to be assigned to a variable.
+##  
+##      >>> enum_object = Enum ("enum name", ["item1", "item2", ...])
+##      >>> enum_object = Enum ("enum name", "item1", "item2", ...)
+##  
+##  the list/args passed become the enumerations items and can be
+##  referenced by parameters or dictionary item reference, and are case
+##  insensitive.
+##  
+##      >>> enum_object.item1
+##      <Enum_Item object at 0x028E3630>
+##      >>> enum_object["iTeM2"]
+##      <Enum_Item object at 0x028E3730>
+##  
+##  Each Enum item is guaranteed to be unique as each item is an object
+##  in memory and will not compare equal with anything other than itself
+##  
+##      >>> enum_object.item1 == "item1"
+##      False
+##      >>> enum_object.item1 == enum_object.item2
+##      False
+##      >>> enum_object.item1 == enum_object["item1"]
+##      True
+##  
+##  Attempting to set values in an enumeration after its creation will
+##  throw an error
+##  
+##      >>> enum_object.item1 = "Foo"
+##      NotImplementedError: 'Enum' object does not support attribute assignment
+##      >>> enum_object["item1"] = "Foo"
+##      TypeError: 'Enum' object does not support item assignment
+##  
+##  Each item in the enumeration can return its name with the `name`
+##  attribute.
+##  
+##      >>> enum_object.item1.name
+##      'item1'
+##  
+##  The name of the enumeration object can be recalled through the use
+##  of the `__name__` method. The enumeration object's name can also be
+##  referenced via an items `type` attribute.
+##  
+##      >>> enum_object.__name__()
+##      'enum name'
+##      >>> enum_object.item1.type
+##      'enum name'
+##----------------------------------------------------------------------
+#Define the enum item class
+class Enum_Item(object):
+    _name = ''
+    _type = ''
+    _initiated = False
+    def __init__(self, name, type):
+        self._name = name
+        self._type = type
+    def __getattr__(self, name):
+        if name == 'name':
+            return self._name
+        if name == 'type':
+            return self._type
+        else: raise AttributeError("No such attribute on 'Enum_Item' object.", name)
+    def __setattr__(self, name, value):
+        if self._initiated:
+            raise NotImplementedError("'Enum_Item' object does not support attribute assignment", name, value)
+        else: super(Enum_Item, self).__setattr__(name, value)
+
+#Define the enum class
+class Enum(object):
+    _d = {}
+    _name = ''
+    _initiated = False
+    def __init__(self, name, *enums):
+        if type(name) != str: raise ValueError("Invalid name for object 'Enum'", name)
+        if type(enums[0]) == list: enums = enums[0]
+        for i in enums:
+            if type(i) != str: raise TypeError("Enum values must be a string.", i)
+            if enums.count(i) > 1: raise ValueError("Duplicate values not allowed.", i)
+            self._d[i.upper().replace(' ','')] = Enum_Item(i, name)
+        self._name = name
+        self._initiated = True
+    def __getattr__(self, a):
+        try:
+            return self._d[a.upper()]
+        except KeyError:
+            raise AttributeError("No enum value defined.", a)
+        else:
+            raise
+    def __setattr__(self, a, v):
+        if self._initiated:
+            raise NotImplementedError("'Enum' object does not support attribute assignment")
+        else: super(Enum, self).__setattr__(a, v)
+    def __getitem__(self, k):
+        return self._d[k.upper().replace(' ','')]
+    def __name__(self):
+        return self._name
